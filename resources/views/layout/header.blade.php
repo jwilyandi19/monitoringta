a122adb7e0d739ea2edee0e213bd4410d441e67d
nafiarwhite <nafiar21@gmail.com>
2017-07-06 06:55:48 +0700

    Tampilan Home Awal

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,18 @@
+<div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #24292e; border : none">
+  <div class="container">
+    <div class="navbar-header">
+      <a href="{{url('/home')}}" class="navbar-brand" style ="color: #ffffff; font-size: 20px;">Sistem Informasi<strong>TA</strong></a>
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="navbar-collapse collapse" id="navbar-main">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="{{url('/hilang')}}" style ="color: #ffffff"> Berita</a></li>
+        <li><a href="{{url('/kalender')}}" class="btn btn-primary" style ="color: #ffffff; padding-top: 10px; padding-bottom: 10px; margin-top: 10px;" > Login</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="panel panel-info">
+	<div class="panel-heading" style="background-color: #2a456f; border-color: #2a456f;">
+		<h5 style="color: #ffffff; margin: 0 0">Berita Terbaru</h5>
+	</div>
+	<div class="panel-body list-group" style="padding: 1px;">
+		<a href="#" class="list-group-item">Kelengkapan yang WAJIB dipenuhi setelah sidang Tugas Akhir GASAL 2016/2017</a>
+		<a href="#" class="list-group-item">Pembagian RMK beserta anggotanya</a>
+		<a href="#" class="list-group-item">Template dan Contoh Proposal TA</a>
+	</div>	
+</div>
+
+<div class="panel panel-info">
+	<div class="panel-heading" style="background-color: #2a456f; border-color: #2a456f;">
+		<h5 style="color: #ffffff; margin: 0 0">Lain - Lain</h5>
+	</div>
+	<div class="panel-body list-group" style="padding: 1px;">
+		<a href="#" class="list-group-item">Teknik Material dan Metalurgi</a>
+		<a href="#" class="list-group-item">Kalender Akademik ITS</a>
+	</div>	
+</div>
