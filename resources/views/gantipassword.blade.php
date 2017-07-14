477b5adef3e072cc4b0f5cec8ff0f34a66397e05
Rafiar Rahmansyah <rafiar254@gmail.com>
2017-07-14 08:53:51 +0700

    Script Responsive Eye Glyph Icon

--- resources/views/gantipassword.blade.php
+++ resources/views/gantipassword.blade.php
@@ -29,23 +29,31 @@ Ganti Password
             <hr>
         </div>
         <div class="panel-body isi-halaman">
-            <form class="form-horizontal col-md-10 col-md-offset-1" method="POST" action="/gantipassword">
+            <form class="form-horizontal col-md-8 col-md-offset-1" method="POST" action="/gantipassword">
                 <fieldset>
                     <div class="form-group has-warning">
-                        <label class="control-label col-md-3" for="judulta" style="text-align: left"><h5>Password Lama</h5></label>
-                        <div class="col-md-9">
-                            <input type="password" name="passwordLama" class="form-control" placeholder="Password lama">
+                        <label class="control-label col-md-3" for="judulta" style="text-align: left"><h6>Password Lama</h6></label>
+                        <div class="col-md-8">
+                            <input id="inputPasswordLama" type="password" name="passwordLama" class="form-control" placeholder="Password lama">
+                        </div>
+                        <div id="show-password" class="col-md-1 pull-right">
+                            <span class="glyphicon glyphicon-eye-open" aria-hidden="true"
+                                  onmousedown="inputPasswordLama.type='text'" onmouseup="inputPasswordLama.type='password'"></span>
                         </div>
                     </div>
                     <div class="form-group has-warning">
-                        <label class="control-label col-md-3" for="judulta" style="text-align: left"><h5>Password Baru</h5></label>
-                        <div class="col-md-9">
-                            <input type="password" name="passwordBaru" class="form-control" placeholder="Password lama">
+                        <label class="control-label col-md-3" for="judulta" style="text-align: left"><h6>Password Baru</h6></label>
+                        <div class="col-md-8">
+                            <input id="inputPasswordBaru" type="password" name="passwordBaru" class="form-control" placeholder="Password lama">
+                        </div>
+                        <div id="show-password" class="col-md-1 pull-right">
+                            <span class="glyphicon glyphicon-eye-open" aria-hidden="true"
+                                  onmousedown="inputPasswordBaru.type='text'" onmouseup="inputPasswordBaru.type='password'"></span>
                         </div>
                     </div>
                     <div class="form-group has-warning">
-                        <label class="control-label col-md-3" for="judulta" style="text-align: left"><h5>Konfirmasi Password</h5></label>
-                        <div class="col-md-9">
+                        <label class="control-label col-md-3" for="judulta" style="text-align: left"><h6>Konfirmasi Password</h6></label>
+                        <div class="col-md-8">
                             <input type="password" name="konfirmasiPassword" class="form-control" placeholder="Password lama">
                         </div>
                     </div>
@@ -53,7 +61,7 @@ Ganti Password
                     {{csrf_field()}}
                     {{method_field('POST')}}
                     <div class="form-group has-warning">
-                        <button type="submit" class="btn btn-primary pull-right"><i class="glyphicon glyphicon-plus"></i> Ajukan Judul</button>
+                        <button type="submit" class="btn btn-primary pull-right">Ganti Password</button>
                     </div>
                 </fieldset>
             </form>
