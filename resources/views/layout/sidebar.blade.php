978c8c38da4e46f4a70431bb3875babe3f81002c
nafiarwhite <nafiar21@gmail.com>
2017-09-10 14:23:45 +0700

    file koordinator

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -7,9 +7,9 @@
 			@if(session('user')['role'] == 1)
 				<a href="{{url('/uploads/Panduan_Aplikasi_Mahasiswa.pdf')}}" target ="_blank" class="list-group-item">Panduan Sistem Informasi Tugas Akhir</a>
 			@elseif(session('user')['role'] == 2)
-				<a href="{{url('/uploads/Panduan-Aplikasi-Dosen.pdf')}}" class="list-group-item">Panduan Sistem Informasi Tugas Akhir</a>
+				<a href="{{url('/uploads/Panduan-Aplikasi-Dosen.pdf')}}" target="_blank" class="list-group-item">Panduan Sistem Informasi Tugas Akhir</a>
 			@elseif(session('user')['role'] == 3)
-				<a href="{{url('/uploads/Panduan-Aplikasi-Koordinator.pdf')}}" class="list-group-item">Panduan Sistem Informasi Tugas Akhir</a>
+				<a href="{{url('/uploads/Panduan-Aplikasi-Koordinator.pdf')}}" target="_blank" class="list-group-item">Panduan Sistem Informasi Tugas Akhir</a>
 			@endif
 			<a href="#" class="list-group-item">Panduan Tugas Akhir</a>
 			<a href="#" class="list-group-item">Prosedur Tugas Akhir</a>
