0526123f464a2ebc3f24742dbe531dfc2261d13c
Jason Wilyandi <jasonwilyandi@yahoo.co.id>
2019-07-12 16:23:58 +0700

    add error 404 message

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Error 404</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
+
+	<link type="text/css" rel="stylesheet" href="{{asset('css/404.css')}}" />
+
+
+</head>
+
+<body>
+
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-404">
+				<h1>404</h1>
+			</div>
+			<h2>Halaman tidak Ditemukan</h2>
+			<p>Pastikan Anda mengetik URL dengan benar. Cek kembali pengecekan url anda</p>
+			<a href="{{url('/')}}">Halaman Utama</a>
+		</div>
+	</div>
+
+</body>
+
+</html>
